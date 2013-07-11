9599f5e6355123b5418e3f5034263f05a842bb6a
Ubuntu <ubuntu@ip-172-31-32-109.us-west-2.compute.internal>
2013-07-11 05:32:34 +0000

    Fixing web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require( fs );
 var app = express.createServer(express.logger());
-var index = fs.readFileSync('./index.html').toString();
+var index = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
   response.send(index);
